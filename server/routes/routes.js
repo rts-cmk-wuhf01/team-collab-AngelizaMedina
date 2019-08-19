600013b89c22bea7ce9557cbd95875b076ce60d0
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-08-19 07:49:56 +0200

    Create a functioning Form (No CSS or Form validation yet)

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -46,5 +46,40 @@ module.exports = (app) => {
 
 	/*----------------------------------------------------- Contact end -------------------------------------------------*/
 
+	app.post('/contact', async (req, res, next) => {
+
+		let name = req.body.name;
+
+		let email = req.body.email;
+
+		let subject = req.body.subject;
+
+		let message = req.body.message;
+
+		let db = await mysql.connect();
+
+		let result = await db.execute(`
+			INSERT INTO messages (
+				message_name,
+				message_email,
+				message_subject,
+				message_message
+			) 
+			VALUES 
+				(?, ?, ?, ?)`, [
+					name,
+					email,
+					subject,
+					message
+				]
+		);
+	
+		db.end();
+
+		res.redirect('/');
+		
+	 }); // app.post('/contact'...) END
+
+
 
 }
