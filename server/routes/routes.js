0c33edda697cba6c98c93692b7e186a70c3ae10f
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-08-20 15:01:45 +0200

    Change a tiny note

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -65,7 +65,7 @@ module.exports = (app) => {
 		
 		db.end();
 
-	}); //app.get('/'.. end)	
+	}); //app.get('/search/:searchInput'.. end)	
 
 	/*---------------------------------------------- Search + searchInput end -------------------------------------------*/
 
